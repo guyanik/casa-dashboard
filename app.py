76700bde0a1ce6cdc6a3df089f2acc4ce131635d
Görkem Uyanık <hgu@macmini.local>
2024-09-20 00:49:47 +0300

    Bug fix

--- app.py
+++ app.py
@@ -155,14 +155,14 @@ with tab2:
 
     if timeframe == "Weekly WO":
         # first day of the week
-        df = df.groupby("LOCUS").resample("W").sum().round(2).reset_index()
+        df = df.groupby("LOCUS").resample("W").sum().round(2)
         df_total = df_total.resample("W").sum().round(2)
     elif timeframe == "Monthly WO":
         # first day of the month
-        df = df.groupby("LOCUS").resample("MS").sum().round(2).reset_index()
+        df = df.groupby("LOCUS").resample("MS").sum().round(2)
         df_total = df_total.resample("MS").sum().round(2)
     elif timeframe == "Yearly WO":
-        df = df.groupby("LOCUS").resample("YS").sum().round(2).reset_index()
+        df = df.groupby("LOCUS").resample("YS").sum().round(2)
         df_total = df_total.resample("YS").sum().round(2)
     wo1 = st.empty()
 
@@ -208,9 +208,8 @@ with tab2:
 
 with tab3:
     df = pd.read_csv("data/daily_wl.csv")
+    df['DATE'] = pd.to_datetime(df['DATE'])
     df_total = df.drop(columns=["LOCUS"]).groupby("DATE").sum().round(2).reset_index()
-    df.index = pd.to_datetime(df["DATE"])
-    df_total.index = pd.to_datetime(df_total['DATE'])
 
     # top-level filters
     col1, col2, col3 = st.columns(3)
@@ -228,15 +227,15 @@ with tab3:
 
     if timeframe == "Weekly WL":
         # first day of the week
-        df = df.groupby("LOCUS").resample("W").sum().round(2).reset_index()
-        df_total = df_total.resample("W").sum().round(2)
+        df = df.set_index('DATE').groupby("LOCUS").resample("W").sum().round(2).reset_index()
+        df_total = df_total.set_index('DATE').resample("W").sum().round(2).reset_index()
     elif timeframe == "Monthly WL":
         # first day of the month
-        df = df.groupby("LOCUS").resample("MS").sum().round(2).reset_index()
-        df_total = df_total.resample("MS").sum().round(2)
+        df = df.set_index('DATE').groupby("LOCUS").resample("MS").sum().round(2).reset_index()
+        df_total = df_total.set_index('DATE').resample("MS").sum().round(2).reset_index()
     elif timeframe == "Yearly WL":
-        df = df.groupby("LOCUS").resample("YS").sum().round(2).reset_index()
-        df_total = df_total.resample("YS").sum().round(2)
+        df = df.set_index('DATE').groupby("LOCUS").resample("YS").sum().round(2).reset_index()
+        df_total = df_total.set_index('DATE').resample("YS").sum().round(2).reset_index()
     
     wl1 = st.empty()
 
